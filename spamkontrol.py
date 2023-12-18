d13b9b2f14916149731290a5a9e0dc7d018228c1
Taha Efe Tuncer <tahaefe303@gmail.com>
2023-12-18 21:26:22 +0300

    .

--- spamkontrol.py
+++ spamkontrol.py
@@ -12,17 +12,16 @@ csvtotxt()
 def spamtespit():
     with open("spamm.txt","r+",encoding="utf-8") as spamtes:       
         icerik = spamtes.read()
-        satırlar = icerik.split("\n")       
+        satırlar = icerik.split("\n")
         return satırlar
-
 
 def spam_mailler():
     with open("spam_mailler.txt","a",encoding="utf-8")as spammail:
-        spammail.write(satir+"\n")
+        spammail.write(icerik+"\n")
 
 def normalmailler():
     with open("normal_mailler.txt","a",encoding="utf-8")as normalmailler:
-        normalmailler.write(satir+"\n")
+        normalmailler.write(icerik+"\n")
 def satır_silici(dosya_adı, satır):
     with open(dosya_adı, 'r', encoding='utf8') as file:
         lines = file.readlines()
@@ -32,45 +31,29 @@ def satır_silici(dosya_adı, satır):
             if index != satır - 1:
                 file.write(line)
 
-while True:
+
    
-    with open("emailtxt.txt","r")as emailveri:
-        satırlar = emailveri.redlines()
-        uzunluk2 = len(satırlar)
-        for i in range(1,uzunluk2):
-            icerik = emailveri.readlines(i)
-            satir = icerik[i-1]
-            uzunluk = len(spamtespit())
-            sayac2 = 0
+with open("emailtxt.txt", "r") as emailveri:
+    satırlar = emailveri.readlines()
+    uzunluk2 = len(satırlar)
 
-            for i in range(0,uzunluk):  
-                yazi = spamtespit()[i]
-                sayac = satir.count(yazi)
-                sayac2 += 1
-                if sayac == 1:
-                    print("spam")
-                    spam_mailler()
-                    satır_silici("emailtxt.txt",i)
-                    break
-                elif sayac2 == 218:
-                    normalmailler()
-                    satır_silici("emailtxt.txt",i)
-                    break
-    
-        
+    for j in range(1, uzunluk2):
+        icerik = satırlar[j - 1]
+        uzunluk = len(spamtespit())
+        sayac2 = 0
 
+        for k in range(0, uzunluk):
+            yazi = spamtespit()[k]
+            sayac = icerik.count(yazi)
+            sayac2 += 1
 
+            if sayac > 0:
+                print("Spam Tespit Edildi")
+                spam_mailler()
+                satır_silici("emailtxt.txt", j)
+                break
+            elif sayac2 == uzunluk:
+                normalmailler()
+                satır_silici("emailtxt.txt", j)
+                break
 
-
-    
-        
-
-
-      
-
-    
-                  
-
-
-    
-    
